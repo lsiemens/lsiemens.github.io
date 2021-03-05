8bedd2920565ac4e9d60b88d80fea987eedcf0bd
Luke Siemens <luke@lsiemens.com>
2021-03-05 11:52:51 -0800

    Reorganized directories and added some unsaved changes

--- theory/fractional_calculus/code/generate2dfc.py
+++ theory/fractional_calculus/code/generate2dfc.py
@@ -4,52 +4,27 @@ from scipy import special
 from matplotlib import pyplot
 from matplotlib.animation import FuncAnimation
 
-resolution = 512 # 512
+fc2dpy.array_dtype=numpy.complex256
+
+resolution = 256 # 512 4096
 order = 128 # 128
 
-PD = fc2dpy.polydisk(0.0 + 0.0j, 0 + -0.0j, 10.0, 10.0, resolution=resolution)
+PD = fc2dpy.polydisk(0.0 + 0.0j, 0.0 + -0.1j, 15.0, numpy.pi, resolution=resolution) #cos
+#PD = fc2dpy.polydisk(0.0 + 1j, 1.5 + -1j, 15.0, 5.0, resolution=resolution)
 PD.set_parameterization(1.0 + 0.0j, 1.0 + 0.0j)
 
 def encoding(a):
+#    return fc2dpy.Gamma(a + 1) ####-------- NICE --------####
 #    return 1.0/fc2dpy.Gamma(-a + 1) ####-------- NICE --------####
-    beta = -1.5
-    return (a)**beta
+#    beta = -numpy.pi/2
+#    return (a)**beta
+    return numpy.cos(a) + numpy.tan(a) # cos
+#    return numpy.exp(-1/(a**3)) # nice
 
-def hypergeometricfunction_2F1_11c(c, x, n=20):
-    """
-    2F1(1, 1; c; x)
-    """
-    out = 0
-    n=128
-    for i in range(n):
-        out += fc2dpy.Gamma(i + 1)*fc2dpy.Gamma(c)*(x)**i/fc2dpy.Gamma(i + c)
-    return out
-
-def iv(v, z, n=10):
-    out = 0
-    for i in range(n):
-        out += (z/2.0)**(2*i + v)/(fc2dpy.Gamma(i + 1)*fc2dpy.Gamma(i + v + 1))
-    return out
-
-def foobar1(x, a, order=None):
-    return x**(a/2)*iv(-a, 2*numpy.sqrt(x))
-
-def foobar2(x, a, order=None):
-    if order is not None:
-        return (x-1)**(a+1)*hypergeometricfunction_2F1_11c(a+2, 1-x, order)/fc2dpy.Gamma(a + 2)
-    else:
-        return (x-1)**(a+1)*hypergeometricfunction_2F1_11c(a+2, 1-x)/fc2dpy.Gamma(a + 2)
-
-def foobar4(x, a, order=None, b=-1.0+0.0j):
-    if order is not None:
-        return (x-b)**(a+1)*hypergeometricfunction_2F1_11c(a+2, 1-x/b, order)/(b*fc2dpy.Gamma(a + 2)) + (x-b)**a*numpy.log(b)/fc2dpy.Gamma(a + 1)
-    else:
-        return (x-b)**(a+1)*hypergeometricfunction_2F1_11c(a+2, 1-x/b)/(b*fc2dpy.Gamma(a + 2)) + (x-b)**a*numpy.log(b)/fc2dpy.Gamma(a + 1)
-
-def foobar3(x, a, order=None):
+def Log(x, a, order=None):
     return (x**a*(numpy.log(x) + special.digamma(1.0) - special.digamma(a + 1))/fc2dpy.Gamma(a + 1))
 
-def foobar5(x, a, order=None):
+def func_inverse_a(x, a, order=None):
     """
     equivelent to function encoding (a)**-1
     """
@@ -63,17 +38,18 @@ def foobar5(x, a, order=None):
         print("complex")
         return numpy.complex(output)
 
+#def foo(x, a, order=None):
+#    return x + 0*a
+
 T = fc2dpy.taylor_encoding(encoding, PD, "A")
 fd = fc2dpy.generator_2d_from_1d(T, order=order)
 
-#fd.function = foobar2
-#fd.function_label = "LOG(1 + (x  - 1))"
-#fd.function = foobar4
-#fd.function_label = "LOG(b + (x  - b))"
-#fd.function = foobar3
+#fd.function = Log
 #fd.function_label = "ln(x)"
-#fd.function = foobar5
+#fd.function = func_inverse_a
 #fd.function_label = "X^a GAMMAINC"
+#fd.function = foo
+#fd.function_label = "X"
 
 pyplot.ylim(-10, 10)
 T.graph_encoding()
@@ -81,4 +57,7 @@ fd.vmax = 1
 fd.vmin = -5
 fd.graph1d_x(a=0.01)
 fd.graph1d_a(x=0.01)
+#fd.g2cxplane()
+fd.graphXplane()
+fd.graphAplane()
 fd.graph2d()
--- theory/fractional_calculus/code/generateanim2dfc.py
+++ theory/fractional_calculus/code/generateanim2dfc.py
@@ -4,39 +4,41 @@ from scipy import special
 from matplotlib import pyplot
 from matplotlib.animation import FuncAnimation
 
-resolution = 256 # 512
-order = 128 # 128
+resolution = 64 # 256
+order = 64 # 128
 
-PD = fc2dpy.polydisk(0, 0, 10, 5, resolution=resolution)
+PD = fc2dpy.polydisk(0, 0, 1, 1, resolution=resolution)
 PD.set_parameterization(1.0 + 0.0j, 1.0 + 0.0j)
+
 def encoding(a):
-    return 1.0/special.gamma(-a + 1) ####-------- NICE --------####
+#    return 1.0/fc2dpy.Gamma(-a + 1) ####-------- NICE --------####
+    beta = 2.0
+    return (a)**beta
 
-def hypergeometricfunction_2F1_11c(c, x, n=10):
+def Log(x, a, order=None):
+    return (x**a*(numpy.log(x) + special.digamma(1.0) - special.digamma(a + 1))/fc2dpy.Gamma(a + 1))
+
+def func_inverse_a(x, a, order=None):
     """
-    2F1(1, 1; c; x)
+    equivelent to function encoding (a)**-1
     """
-    out = 0
-    for i in range(n):
-        out += special.gamma(i + 1)*special.gamma(c)*(x)**i/special.gamma(i + c)
-    return out
+    import mpmath
+    gammaincomplete = numpy.frompyfunc(mpmath.gammainc, 2, 1)
+    output = (-x)**a*(gammaincomplete(-a, -x) - special.gamma(-numpy.array(a, dtype=numpy.complex128)))
+    if hasattr(x, "__len__") or hasattr(a, "__len__"):
+        print("array")
+        return numpy.array(output, dtype=fc2dpy.array_dtype)
+    else:
+        print("complex")
+        return numpy.complex(output)
 
-def iv(v, z, n=10):
-    out = 0
-    for i in range(n):
-        out += (z/2.0)**(2*i + v)/(special.gamma(i + 1)*special.gamma(i + v + 1))
-    return out
-
-#def foobar(x, a, order=None):
-#    return x**(a/2)*iv(-a, 2*numpy.sqrt(x))
-
-def foobar(x, a, order=None):
-    return x**(a+1)*hypergeometricfunction_2F1_11c(a+2, -x)/special.gamma(a + 2)
-
-T = fc2dpy.taylor_encoding(encoding, PD, "cos(a)/(a-1)")
+T = fc2dpy.taylor_encoding(encoding, PD, "A")
 fd = fc2dpy.generator_2d_from_1d(T, order=order)
 
-fd.function = foobar
+#fd.function = Log
+#fd.function_label = "ln(x)"
+#fd.function = func_inverse_a
+#fd.function_label = "X^a GAMMAINC"
 
 pyplot.ylim(-10, 10)
 T.graph_encoding()
@@ -46,30 +48,38 @@ fd.graph1d_x()
 fd.graph1d_a(x=0.0)
 fd.graph2d()
 
-n = 30*60 #30*60
+fps = 30
+number_of_frames = 30*6 #30*60
 data = []
-for i in range(n):
-    print(i, n)
-    speed = 2*numpy.pi/n
+for i in range(number_of_frames):
+    print(i, number_of_frames)
+    speed = 2*numpy.pi/number_of_frames
 
-#    fd.polydisk.set_parameterization(1.0, numpy.cos(speed*i) + 1.0j*numpy.sin(speed*i))
+    fd.polydisk.set_parameterization(1.0, numpy.cos(speed*i) + 1.0j*numpy.sin(speed*i))
 #    fd.polydisk.set_parameterization(numpy.cos(speed*i) + 1.0j*numpy.sin(speed*i), 1.0)
-    fd.polydisk.set_parameterization(numpy.cos(2*speed*i) + 1.0j*numpy.sin(2*speed*i), numpy.cos(3*speed*i) + 1.0j*numpy.sin(3*speed*i))
 #    temp = numpy.log(numpy.abs(fd.function(fd.polydisk.Grid_x, fd.polydisk.Grid_a, fd.order))) #1
-    temp = numpy.angle(fd.function(fd.polydisk.Grid_x, fd.polydisk.Grid_a, fd.order)) #2
+#    temp = numpy.angle(fd.function(fd.polydisk.Grid_x, fd.polydisk.Grid_a, fd.order)) #2
+    temp = fd.function(fd.polydisk.Grid_x, fd.polydisk.Grid_a, fd.order) #2
     data.append(temp)
 i = 0
 
-vmax, vmin = numpy.nanmax(data), numpy.nanmin(data)
+vmax, vmin = numpy.nanmax(numpy.abs(data)), numpy.nanmin(numpy.abs(data))
 print(vmax, vmin)
 
 fig = pyplot.figure()
 
 def foo(i):
     pyplot.clf()
+    def mapping(data, vmin, vmax):
+        out = numpy.zeros(shape=data.shape + (3,))
+        out[:,:,0] = numpy.clip((numpy.real(data) - vmin)/(vmax - vmin), 0, 1)
+        out[:,:,1] = numpy.clip((numpy.imag(data) - vmin)/(vmax - vmin), 0, 1)
+        out[:,:,2] = 0.0*numpy.real(data)
+        return out
 #    pyplot.imshow(data[i%len(data)], vmin=-vmax, vmax=vmax, interpolation="bicubic", cmap="magma") #1
-    pyplot.imshow(data[i%len(data)], vmin=-vmax, vmax=vmax, interpolation="bicubic", cmap="plasma") #2
+#    pyplot.imshow(data[i%len(data)], vmin=-vmax, vmax=vmax, interpolation="nearest", cmap="hsv") #2
+    pyplot.imshow(mapping(data[i%len(data)], vmin, vmax), interpolation="nearest") #2
 
-ani = FuncAnimation(fig, foo, frames=n, interval=16)
+ani = FuncAnimation(fig, foo, frames=number_of_frames, interval=1000/fps)
 ani.save("anim.mp4")
 pyplot.show()
